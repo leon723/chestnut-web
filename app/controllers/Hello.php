462384792ddc1bf1853e438233a3b78f7203a7c8
LeonZhang <33543015@qq.com>
2015-09-13 10:32:46 +0800

    Update to 0.4.0
    Update to 0.4.0

--- app/controllers/Hello.php
+++ app/controllers/Hello.php
@@ -1,9 +1,6 @@
 <?php
 namespace App\Controllers;
 
-use App\Models\User;
-use App\Models\UserProfile;
-
 class Hello{
   public function hello() {
     return view('hello', ['chestnut'=> 'Chestnut']);
